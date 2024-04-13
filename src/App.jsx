a13060f5f6d2a3529b5c1359736e74136ab9cca5
Fluebubble <Trots.anatoly@gmail.com>
2024-04-13 15:57:04 +0300

    eslint fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { TestComponent } from './components/TestComponent/TestComponent';
 import 'modern-normalize';
+import { Button } from './components/Button/Button';
 
 export const App = () => {
   const [counter, setCounter] = useState(0);
@@ -8,7 +9,7 @@ export const App = () => {
   return (
     <>
       <p>Counter {counter}</p>
-
+      <Button>Users</Button>
       <button
         type="button"
         onClick={() => setCounter((prevState) => prevState + 1)}
